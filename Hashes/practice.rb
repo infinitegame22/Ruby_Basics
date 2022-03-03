e6df7cbe7cdd4347e013b314ef1e93467bc91034
Amy Dripchak <adripchak@yahoo.com>
2022-03-02 18:23:09 -0700

    add more files

--- Hashes/practice.rb
+++ Hashes/practice.rb
@@ -9,3 +9,16 @@ low_numbers = numbers.select do |key, value|
 end
 
 p low_numbers
+
+numbers = {
+  low:  5,
+  medium: 10,
+  high:   15
+}
+
+low_numbers = numbers.select do |key, value|
+  value < 25
+end
+
+p low_numbers
+p numbers
